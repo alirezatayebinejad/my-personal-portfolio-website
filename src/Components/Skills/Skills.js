1ccf26e0de350fb7fff341d5bf957fbc14562299
vibracode <codevibra@gmail.com>
2023-07-08 09:12:31 +0330

    css performance optimized

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -11,19 +11,25 @@ const Skills = () => {
         { imgUrl: "/Assets/skills/css.png", title: "CSS" },
         { imgUrl: "/Assets/skills/javascript.png", title: "Javascript" },
         { imgUrl: "/Assets/skills/react.png", title: "React" },
+        { imgUrl: "/Assets/skills/nextjs.png", title: "Next.js" },
         { imgUrl: "/Assets/skills/redux.png", title: "Redux" },
+        { imgUrl: "/Assets/skills/mysql.png", title: "MySQL" },
         { imgUrl: "/Assets/skills/materialui.png", title: "Material UI" },
         { imgUrl: "/Assets/skills/styledcomponents.png", title: "Styled Components" },
         { imgUrl: "/Assets/skills/bootstrap.png", title: "Bootstrap" },
         { imgUrl: "/Assets/skills/firebase.png", title: "Firebase" },
         { imgUrl: "/Assets/skills/wordpress.png", title: "Wordpress" },
     ]
+    const renderCards = () => {
+        const cards = skillsData.map((skill) => <SkillsCard {...skill} key={skill.title} />);
+        return cards;
+    }
     return (
         <div className='skills' id='skills1'>
             <h1 className="skills__header">Tech Skills</h1>
             <div className="skills__items">
 
-                {skillsData.map((skill) => <SkillsCard {...skill} key={skill.title} />)}
+                {renderCards()}
 
             </div>
         </div>
